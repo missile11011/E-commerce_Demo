830848b2f45edd7e0431d8a69cdbc2485d63cfc4
Misael Reyes <misaelrey90@gmail.com>
2022-01-09 09:22:26 -0600

    apollo and graphql setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,49 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Nav from "./components/Nav"
 import Home from "./pages/Home.js"
 import Footer from "./components/Footer"
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
 
   return (
-    <Router>
-      <Nav/>
-      <Switch>
-        <Route exact path='/' component={Home} />
-      </Switch>
-      <Footer/>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <Nav/>
+        <Switch>
+          <Route exact path='/' component={Home} />
+        </Switch>
+        <Footer/>
+      </Router>
+    </ApolloProvider>
   );
 }
 
--- server/server.js
+++ server/server.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const path = require('path');
+const { ApolloServer } = require('apollo-server-express');
 
+const db = require("./config/connection")
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+const {authMiddleware} = require("./utils/auth")
+const { typeDefs, resolvers } = require('./schema');
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -10,17 +22,20 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 // Serve up static assets
-// app.use('/images', express.static(path.join(__dirname, '../client/public')));
+app.use('/images', express.static(path.join(__dirname, '../client/public')));
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
-// }
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
-// });
-
-
-app.listen(PORT, () => {
-  console.log(`API server running on port ${PORT}!`);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
+
+
+db.once("open", ()=>{
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+})
