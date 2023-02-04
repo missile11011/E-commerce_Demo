2fd4bb40fcd891acdc9cfa53b5d9c92a85ac853e
Misael Reyes <misaelrey90@gmail.com>
2023-02-04 10:10:59 -0600

    server side Stripe api requests, and client side checkout page setup for Stripe Checkout Session

--- client/src/pages/Checkout.js
+++ client/src/pages/Checkout.js
@@ -1,21 +1,17 @@
-import React, { useState, useEffect, Component } from "react";
-import { loadStripe } from "@stripe/stripe-js";
-import { Elements } from "@stripe/react-stripe-js";
-import { useSelector } from "react-redux";
+import React, {useState, useEffect, Component} from "react";
+import {loadStripe} from "@stripe/stripe-js";
+import {Elements} from "@stripe/react-stripe-js";
+import {useSelector} from "react-redux";
 import CheckoutForm from "../components/CheckoutForm";
 import ShippingForm from "../components/ShippingForm";
 import Auth from "../utils/auth";
-
-const stripePromise = loadStripe(
-	"pk_test_51KYfOOHOMpPyIznJJw8smgIiP7aaOygzJAcRCiNvZYzZq0lz5NJumR9QnhS1xZCSf1KcPAUYKZHo2tU2NmlLTJvO00kR88grXB"
-);
 
 export default function Checkout() {
 	const [clientSecret, setClientSecret] = useState("");
 	const [total, setTotal] = useState(0);
 	const [stage, setStage] = useState(1);
 	const state = useSelector((state) => state);
-	const { cart } = state;
+	const {cart} = state;
 
 	useEffect(() => {
 		const shippingCost = parseFloat(Object.keys(cart).length * 12);
@@ -28,26 +24,11 @@ export default function Checkout() {
 			return subTotalNum;
 		};
 		setTotal(parseFloat(shippingCost) + parseFloat(subTotal()));
-		console.log("total", total)
-		fetch("/create-payment-intent", {
-		method: "POST",
-		headers: { "Content-Type": "application/json" },
-		body: JSON.stringify({ total: parseInt((total*100).toFixed(2)) }),
-		})
-		.then((res) => res.json())
-		.then((data) => setClientSecret(data.clientSecret));
 	}, [total]);
 	
-	const appearance = {
-		theme: "stripe",
-	};
-	const options = {
-		clientSecret,
-		appearance,
-	};
-
 	const handleSubmit = () => {
-		setStage((stage) => stage + 1);
+		setStage(stage + 1);
+		stagePosition();
 	};
 	const stagePosition = () => {
 		if (stage === 1) {
@@ -59,7 +40,10 @@ export default function Checkout() {
 				<div class="col text-center">
 					<h1>Not logged in</h1>
 					<div>
-						<button class="btn btn-primary m-2">
+						<button
+							class="btn btn-primary m-2"
+							onClick={handleSubmit}
+						>
 							Continue as geust
 						</button>
 						<p>or</p>
@@ -81,13 +65,12 @@ export default function Checkout() {
 			);
 		} else if (stage === 3) {
 			return (
-				<div>
+				<div class="col-6 m-3">
 					<div className="App col-5 mx-auto">
-						{clientSecret && (
-							<Elements options={options} stripe={stripePromise}>
-								<CheckoutForm total={total} />
-							</Elements>
-						)}
+						<h3>Paymemt</h3>
+						<form action="/create-checkout-session" method="POST">
+							<button type="submit">Checkout</button>
+						</form>
 					</div>
 				</div>
 			);
--- server/server.js
+++ server/server.js
@@ -1,63 +1,78 @@
-const express = require('express');
-const path = require('path');
-const { ApolloServer } = require('apollo-server-express');
+const express = require("express");
+const path = require("path");
+const {ApolloServer} = require("apollo-server-express");
 
-const db = require("./config/connection.js")
+const db = require("./config/connection.js");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const router = express.Router();
-const {authMiddleware} = require("./utils/auth")
-const { typeDefs, resolvers } = require('./schema');
+const {authMiddleware} = require("./utils/auth");
+const {typeDefs, resolvers} = require("./schema");
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware
+	typeDefs,
+	resolvers,
+	context: authMiddleware,
 });
+require("dotenv").config({path: "../.env"});
 
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require("stripe")(process.env.STRIPE_API_KEY);
+server.applyMiddleware({app});
 
-server.applyMiddleware({ app });
-
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "public")));
 
 // Serve up static assets
-app.use('/images', express.static(path.join(__dirname, '../client/public')));
+app.use("/images", express.static(path.join(__dirname, "../client/public")));
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
-app.post("/create-payment-intent", async (req, res) => {
-  try{
-    const {total} = req.body 
-    const paymentIntent = await stripe.paymentIntents.create({
-    amount: total,
-    currency: 'usd',
-    payment_method_types: ['card'],
-  });
-  res.send({clientSecret: paymentIntent.client_secret})
-  }
-  catch(e){
-    return res.status(400).send(e.message);
-  }
-  console.log("payment-intent route")
+app.post("/create-checkout-session", async (req, res) => {
+	const session = await stripe.checkout.sessions.create({
+		line_items: [
+			{
+				price: "price_1MPIqUHOMpPyIznJWh9t4G8g",
+				quantity: 1,
+			},
+			{
+				price: "price_1MPIqUHOMpPyIznJWh9t4G8g",
+				quantity: 1,
+			},
+		],
+		mode: "payment",
+		success_url: `http://localhost:3000/checkout`,
+		cancel_url: `http://localhost:3000/test`,
+		automatic_tax: {enabled: true},
+	});
+	res.redirect(303, session.url);
 });
 
-app.post("/config", (req,res) => {
-  console.log("config route")
-  res.send({stripeKey: process.env.STRIPE_SECRET_KEY})
+app.post("/create-product", async (req, res) => {
+	const product = await stripe.products.create({
+		name: req.body.name,
+		description: req.body.description,
+		images: req.body.image,
+		default_price_data: {
+			currency: "USD",
+			unit_amount_decimal: req.body.price,
+		},
+	});
+	res.send(product);
 });
-
-db.once("open", ()=>{
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-  });
-})
+// app.post("/config", (req,res) => {
+//   console.log("config route")
+//   res.send("a;sdlkfja;sd")
+// });
+db.once("open", () => {
+	app.listen(PORT, () => {
+		console.log(`API server running on port ${PORT}!`);
+		console.log(`App running on http://localhost:${PORT}`);
+	});
+});
